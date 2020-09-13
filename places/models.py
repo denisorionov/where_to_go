09edb08bcbe8f0ff7614a853fb41471cd6c480af
Denis Orionov <orionovdv@gmail.com>
2020-09-13 09:12:28 +0300

    Add db_index

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField('Название', max_length=100)
     short_description = models.TextField('Краткое описание', max_length=400, blank=True)
     long_description = tinymce_models.HTMLField('Описание', blank=True)
-    lng = models.FloatField('Долгота')
-    lat = models.FloatField('Широта')
+    lng = models.FloatField('Долгота', db_index=True)
+    lat = models.FloatField('Широта', db_index=True)
 
     class Meta:
         verbose_name_plural = 'Локации'  # название модели в множественном числе
@@ -17,7 +17,7 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    position = models.PositiveIntegerField('Позиция', default=0, null=True, blank=True)
+    position = models.PositiveIntegerField('Позиция', default=0, null=True, blank=True, db_index=True)
     img = models.ImageField('Фото', upload_to='picture')
     place = models.ForeignKey(Place, verbose_name='Локация', on_delete=models.CASCADE, related_name='images', blank=True)
 
